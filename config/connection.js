ddc90e36141228164bfee186d2a787eb8c698ba5
unknown <efinley67@gmail.com>
2017-10-29 21:12:44 -0600

    Updated HW files

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "20IsftD25",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL_;)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "20IsftD25",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
